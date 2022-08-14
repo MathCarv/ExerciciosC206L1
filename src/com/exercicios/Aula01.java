6bcab2577c57f091f9d37403905b80dd20e0fffe
MathCarv <matheus.camara@gec.inatel.br>
2022-08-14 20:58:22 -0300

    Exercicios de Aula

---
+++ src/com/exercicios/Aula01.java
@@ -0,0 +1,45 @@
+package com.exercicios;
+import java.util.Scanner;
+import java.lang.Math;
+
+public class Aula01 {
+    public static void main(String[] args) {
+        double primeironumero, segundonumero, resultado;
+        String escolha;
+        Scanner ler = new Scanner(System.in);
+        System.out.print("Digite o primeiro numero: ");
+        primeironumero = ler.nextDouble();
+        System.out.print("Digite o segundo numero: ");
+        segundonumero = ler.nextDouble();
+        System.out.print("Digite a operacao desejada: " +
+                    "\n1 - Soma" +
+                    "\n2 - Subtracao" +
+                    "\n3 - Multiplicacao" +
+                    "\n4 - Divisao" +
+                    "\n5 - Exponencial" +
+                    "\n-> ");
+            escolha = ler.next();
+            switch (escolha) {
+                case "Soma":
+                    resultado = primeironumero + segundonumero;
+                    System.out.println("O resulta da soma e: " + resultado);
+                    break;
+                case "Subtracao":
+                    resultado = primeironumero - segundonumero;
+                    System.out.println("O resulta da Subtracao e: " + resultado);
+                    break;
+                case "Multiplicacao":
+                    resultado = primeironumero * segundonumero;
+                    System.out.println("O resulta da Multiplicacao e: " + resultado);
+                    break;
+                case "Divisao":
+                    resultado = primeironumero / segundonumero;
+                    System.out.println("O resulta da Divisao e: " + resultado);
+                    break;
+                case "Exponencial":
+                    resultado = Math.pow(primeironumero,segundonumero);
+                    System.out.println("O resulta do Exponencial e: " + resultado);
+                    break;
+            }
+    }
+}
